fd9c45cc7062fcc90e820dfd62eeae500210886a
Diou Medeiros <diou.182@gmail.com>
2020-05-04 23:48:22 -0300

    feat: Initialized routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import React from "react";
-import { View, StatusBar, Text } from "react-native";
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
-    <StatusBar barStyle={"light-content"} backgroundColor="#312e38" />
-    <View style={{ backgroundColor: "#312e38", flex: 1 }}>
-      <Text
-        style={{
-          color: "#fff",
-          textAlign: "center",
-        }}
-      >
-        Diou
-      </Text>
+  <NavigationContainer>
+    <StatusBar barStyle={'light-content'} backgroundColor="#312e38" />
+    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+      <Routes />
     </View>
-  </>
+  </NavigationContainer>
 );
 
 export default App;
